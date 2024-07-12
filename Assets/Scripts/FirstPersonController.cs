5c10aa911fa717e56443a5725378e02f70491081
Anvi Penmetsa <anvipenmetsa@gmail.com>
2024-07-12 12:57:41 -0700

    Add jumping to player controls

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
+    [SerializeField] private float playerHeight = 2f;
+
     public float mouseSensitivityX = 250f;
     public float mouseSensitivityY = 250f;
     public float walkSpeed = 8f;
+    public float jumpForce = 220f;
+    public LayerMask groundedMask;
 
     Transform cameraT;
     float verticalLookRotation;
 
     Vector3 moveAmount;
     Vector3 smoothMoveVelocity;
+
+    bool grounded;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +37,23 @@ public class FirstPersonController : MonoBehaviour
         Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         Vector3 targetMoveAmount = moveDir * walkSpeed;
         moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, 0.15f);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (grounded)
+            {
+                GetComponent<Rigidbody>().AddForce(transform.up * jumpForce);
+            }
+        }
+
+        grounded = false;
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, (playerHeight / 2) + 0.1f, groundedMask))
+        {
+            grounded = true;
+        }
     }
 
     void FixedUpdate()
